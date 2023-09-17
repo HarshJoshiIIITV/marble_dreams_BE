20482a9e96e68513cf74014dab88b8f9de42c31c
HarshJoshiIIITV <joshih1998@gmail.com>
2023-09-17 15:18:45 +0530

    adding initial files

--- routes/Auth.js
+++ routes/Auth.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 const router = express.Router();
 //  /auth is already added in base path
-router.post('/signup', createUser)
+router.post('/signup', createUser) 
 .post('/login', passport.authenticate('local'), loginUser)
 .get('/check',passport.authenticate('jwt'), checkAuth)
 .get('/logout', logout)
